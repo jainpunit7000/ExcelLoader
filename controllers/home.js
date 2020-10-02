6d35d8a5c9cfa197ece099910e742a1f505e9f00
jainpunit7000 <jainpunit7000@gmail.com>
2020-10-02 15:08:24 +0530

    Latest updates

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,8 @@
 const readXlsxFile = require('read-excel-file/node');
 const fs = require("fs") ;
 const Candidate = require("../models/candidate") ;
-const each = require('async-each-series');
-
+// const each = require('async-each-series');
+const async = require("async") ;
 
 exports.getHome = (req,res,next) => {
     res.render("home") ;
@@ -44,6 +44,7 @@ exports.postUpload = (req,res,next) => {
             'annual salary',
             'education'
         ]
+        const emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         if( rows[0].length != 13 ){
             res.redirect("/fileError") ;
         } else {
@@ -51,137 +52,183 @@ exports.postUpload = (req,res,next) => {
                 if( rows[0][i].toLowerCase() !== dataFormat[i] )
                     res.redirect("/fileError") ;
             }
-            ///////////
-            // const data = [] ;
-            // for(let i=1;i<rows.length;++i)
-            //     data.push(rows[i]) ;
-            // let inserts=0 ;
+            /////////// start 
+            const data = [] ;
+            for(let i=1;i<rows.length;++i)
+                data.push(rows[i]) ;
+            let inserts=0 ;
             // console.log("starting") ;
-            // each(data, (ele,next) => {
-            //     Candidate.uniqueEmails([ele[4]])
-            //         .then( result => {
-            //             console.log(result) ;
-            //             if( !result ){
-            //                 let name = ele[0];
-            //                 let email =  ele[4];
-            //                 if( !name || !email )
-            //                     return ;
-            //                 let address = ele[1];
-            //                 let mobileNo = ele[2];
-            //                 let dob = ele[3];
-            //                 let experience = ele[5];
-            //                 experience = {
-            //                     years : experience.split(" ")[0] || "0" ,
-            //                     months : experience.split(" ")[2] || "0" 
-            //                 }
-            //                 let resumeTitle = ele[6];
-            //                 let cLocation = ele[7];
-            //                 let pLocation = ele[8];
-            //                 let cEmployer = ele[9];
-            //                 let cDesignation = ele[10];
-            //                 let salary = ele[11].split(" ")[1];
-            //                 salary = parseFloat(salary) ;
-            //                 salary = parseInt(salary*100000) ;
-            //                 let education = ele[12].split(",");
-            //                 const candidate = new Candidate(
-            //                     name,address,mobileNo,dob,email,experience,resumeTitle,cLocation,pLocation,cEmployer,cDesignation,salary,education
-            //                 )
-            //                 console.log(candidate) ;
-            //                 inserts++ ;
-            //                 candidate.save()
-            //                     .then( result => {
-            //                         // console.log(result) ;
-            //                     } )
-            //                     .catch( err => {
-            //                         console.log(err) ;
-            //                     } )
-            //             }
-            //         })
-            //         .catch(err => {
-            //             console.log(err) ;
-            //         })
-            //     next() ;
-            // }, (err) => {
-            //     res.redirect("/success/" + inserts + "-" + (rows.length-1) ) ;
-            // } )
+            async.each(data, (ele,callback) => {
+                if (!emailFilter.test(ele[4] || "NA")){
+                    // console.log("invalid email " + ele[4]) ;
+                    callback() ;
+                    return ;
+                }
+                Candidate.uniqueEmails([ele[4]])
+                    .then( result => {
+                        // console.log(result) ;
+                        if( result.length == 0 ){
+                            // console.log("here") ;
+                            let name = ele[0];
+                            let email =  ele[4];
+                            if( !(name||email) ){
+                                callback() ;
+                                return ;
+                            }
+                            
+                            let address = ele[1] || "NA";
+                            let mobileNo = ele[2] || "NA";
+                            let dob = ele[3] || "NA";
+                            let resumeTitle = ele[6] || "NA";
+                            let cLocation = ele[7] || "NA";
+                            let pLocation = ele[8] || "NA";
+                            let cEmployer = ele[9] || "NA";
+                            let cDesignation = ele[10] || "NA";
+                            let salary = ele[11] || "NA";
+                            let experience = ele[5] || "NA";
+                            let education = ele[12] || "NA";
+                            if( salary !== "NA" ){
+                                salary = salary.split(" ")[1] ;
+                                salary = parseFloat(salary) ;
+                                salary = parseInt(salary*100000) ;
+                            }
+                            if( education !== "NA" ){
+                                education = education.split(",") ;
+                            }
+                            if( experience == "NA" ){
+                                experience = {
+                                    years : "0" ,
+                                    months : "0" 
+                                }
+                            } else {
+                                experience = {
+                                    years : experience.split(" ")[0] || "0" ,
+                                    months : experience.split(" ")[2] || "0" 
+                                }
+                            }
+                            const candidate = new Candidate(
+                                name,address,mobileNo,dob,email,experience,resumeTitle,cLocation,pLocation,cEmployer,cDesignation,salary,education
+                            )
+                            // console.log(candidate) ;
+                            inserts++ ;
+                            candidate.save()
+                                .then( result => {
+                                    callback() ;
+                                    // console.log(result) ;
+                                } )
+                                .catch( err => {
+                                    console.log(err) ;
+                                } )
+                        } else {
+                            callback() ;
+                            return ;
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err) ;
+                    })
+            }, (err) => {
+                res.redirect("/success/" + inserts + "-" + (rows.length-1) ) ;
+            } )
 
             ///////////////
 
-            const unqRows = [] ;
-            const unqEmails = [] ;
-            for(let i=1;i<rows.length;++i){
-                if( rows[i].length != 13 )
-                    continue ;
-                let name = rows[i][0];
-                let email = rows[i][4];
-                if( !name || !email )
-                    continue ;
-                let unique = true ;
-                for(let j=0;j<unqEmails.length;++j){
-                    if( email == unqEmails[j] ){
-                        unique = false ;
-                        break ;
-                    }
-                }
-                if( !unique )
-                    continue ;
-                let address = rows[i][1];
-                let mobileNo = rows[i][2];
-                let dob = rows[i][3];
-                let experience = rows[i][5];
-                experience = {
-                    years : experience.split(" ")[0] || "0" ,
-                    months : experience.split(" ")[2] || "0" 
-                }
-                let resumeTitle = rows[i][6];
-                let cLocation = rows[i][7];
-                let pLocation = rows[i][8];
-                let cEmployer = rows[i][9];
-                let cDesignation = rows[i][10];
-                let salary = rows[i][11].split(" ")[1];
-                salary = parseFloat(salary) ;
-                salary = parseInt(salary*100000) ;
-                let education = rows[i][12].split(",");
-                unqEmails.push(email) ;
-                unqRows.push([name,address,mobileNo,dob,email,experience,resumeTitle,cLocation,pLocation,cEmployer,cDesignation,salary,education]) ;
-            }
-            Candidate.uniqueEmails(unqEmails)
-            .then( matches => {
-                // console.log(unqEmails) ;
-                // console.log(matches) ;
-                let insertedRows = unqEmails.length - matches.length ;
-                if( insertedRows <= 0 ) 
-                    insertedRows = 0 ;
-                for(let i=0;i<unqEmails.length;++i){
-                    let canInsert = true ;
-                    for(let j=0;j<matches.length;++j){
-                        if( unqEmails[i] == matches[j].email ){
-                            canInsert = false ;
-                            break ;
-                        }
-                    }
-                    if( !canInsert )
-                        continue ;
-                    const candidate = new Candidate(
-                        ...unqRows[i]
-                    ) ;
-                    console.log(unqRows[i]) ;
-                    console.log(candidate) ;
-                    candidate.save()
-                        .then(result => {
-                            // console.log(result) ;
-                        })
-                        .catch(err => {
-                            console.log(err) ;
-                        });
-                }
-                res.redirect("/success/" + insertedRows + "-" + (rows.length-1) ) ;
-                    // console.log(matches) ;
-                } )
-            .catch( err => {
-                console.log(err) ;
-            })
-            
+            // const unqRows = [] ;
+            // const unqEmails = [] ;
+            // for(let i=1;i<rows.length;++i){
+            //     if( rows[i].length != 13 )
+            //         continue ;
+            //     let name = rows[i][0];
+            //     let email = rows[i][4];
+            //     if( !name || !email )
+            //         continue ;
+            //     if (!emailFilter.test(email)){
+            //         // console.log("invalid email " + ele[4]) ;
+            //         continue ;
+            //     }
+            //     let unique = true ;
+            //     for(let j=0;j<unqEmails.length;++j){
+            //         if( email == unqEmails[j] ){
+            //             unique = false ;
+            //             break ;
+            //         }
+            //     }
+            //     if( !unique )
+            //         continue ;
+            //     let address = rows[i][1] || "NA";
+            //     let mobileNo = rows[i][2] || "NA";
+            //     let dob = rows[i][3] || "NA";
+            //     let experience = rows[i][5] || "NA";
+            //     let resumeTitle = rows[i][6] || "NA";
+            //     let cLocation = rows[i][7] || "NA";
+            //     let pLocation = rows[i][8] || "NA";
+            //     let cEmployer = rows[i][9] || "NA";
+            //     let cDesignation = rows[i][10] || "NA";
+            //     let salary = rows[i][11] || "NA";
+            //     let education = rows[i][12] || "NA";
+            //     if( !( name && email &&address && mobileNo && dob && experience && resumeTitle && cLocation &&
+            //         pLocation && cEmployer && cDesignation && salary && education ) ){
+            //         continue ;
+            //     }
+            //     if( salary !== "NA" ){
+            //         salary = salary.split(" ")[1] ;
+            //         salary = parseFloat(salary) ;
+            //         salary = parseInt(salary*100000) ;
+            //     }
+            //     if( education !== "NA" ){
+            //         education = education.split(",") ;
+            //     }
+            //     if( experience == "NA" ){
+            //         experience = {
+            //             years : "0" ,
+            //             months : "0" 
+            //         }
+            //     } else {
+            //         experience = {
+            //             years : experience.split(" ")[0] || "0" ,
+            //             months : experience.split(" ")[2] || "0" 
+            //         }
+            //     }
+            //     unqEmails.push(email) ;
+            //     unqRows.push([name,address,mobileNo,dob,email,experience,resumeTitle,cLocation,pLocation,cEmployer,cDesignation,salary,education]) ;
+            // }
+            // Candidate.uniqueEmails(unqEmails)
+            // .then( matches => {
+            //     // console.log(unqEmails) ;
+            //     // console.log(matches) ;
+            //     let insertedRows = unqEmails.length - matches.length ;
+            //     if( insertedRows <= 0 ) 
+            //         insertedRows = 0 ;
+            //     for(let i=0;i<unqEmails.length;++i){
+            //         let canInsert = true ;
+            //         for(let j=0;j<matches.length;++j){
+            //             if( unqEmails[i] == matches[j].email ){
+            //                 canInsert = false ;
+            //                 break ;
+            //             }
+            //         }
+            //         if( !canInsert )
+            //             continue ;
+            //         const candidate = new Candidate(
+            //             ...unqRows[i]
+            //         ) ;
+            //         // console.log(unqRows[i]) ;
+            //         // console.log(candidate) ;
+            //         candidate.save()
+            //             .then(result => {
+            //                 // console.log(result) ;
+            //             })
+            //             .catch(err => {
+            //                 console.log(err) ;
+            //             });
+            //     }
+            //     res.redirect("/success/" + insertedRows + "-" + (rows.length-1) ) ;
+            //         // console.log(matches) ;
+            //     } )
+            // .catch( err => {
+            //     console.log(err) ;
+            // })
+            //end
         }
         })
     .catch( err =>{
